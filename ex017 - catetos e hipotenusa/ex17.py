0d6e749628cb8d433686606df51a1b731bb9f4e9
valdir-amaral <valdir.amaral58@gmail.com>
2023-02-23 11:47:06 -0300

    catetos e hipotenusa update

--- ex017 - catetos e hipotenusa/ex17.py
+++ ex017 - catetos e hipotenusa/ex17.py
@@ -1,9 +1,5 @@
-from math import pow, sqrt
-
-def calc_hipotenusa(c1, c2):
-    hipotenusa = sqrt(pow(c1, 2) + pow(c2, 2))
-    return hipotenusa
+from math import hypot
 
 cateto1 = int(input('Digite o valor do primeiro cateto: '))
 cateto2 = int(input('Digite o valor do segundo cateto: '))
-print(f'A hipotenusa vale {calc_hipotenusa(cateto1, cateto2):.2f}')
+print(f'A hipotenusa vale {hypot(cateto1, cateto2):.2f}')
