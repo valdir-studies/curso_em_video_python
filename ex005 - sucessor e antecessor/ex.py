57c5590ed6c391da83cb6ec14145756c81cd8104
valdir-amaral <valdir.amaral58@gmail.com>
2023-02-23 09:52:03 -0300

    ex 05, 06 e 07

---
+++ ex005 - sucessor e antecessor/ex.py
@@ -0,0 +1,4 @@
+def sucessor_antecessor(n):
+    print(f'O antecessor de {n} é {n-1} e o sucessor é {n+1}')
+    
+sucessor_antecessor(7) # O antecessor de 7 é 6 e o sucessor é 8
---
+++ ex006 - dobro triplo raiz/ex06.py
@@ -0,0 +1,3 @@
+n = int(input('Insira um número inteiro: '))
+
+print(f'O dobro de {n} é {n*2}, o triplo é {n*3} e a raiz quadrada é {n**(1/2):.2f}')
