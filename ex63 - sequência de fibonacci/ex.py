5e7d12ba6899220777a637d1b6dd0985348d4e36
valdir-amaral <valdir.amaral58@gmail.com>
2023-03-08 14:20:31 -0300

    sequência de fibonacci

---
+++ ex63 - sequência de fibonacci/ex.py
@@ -0,0 +1,16 @@
+# Exercício Python 63: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. Exemplo:
+
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+
+print(f'{t1} -> {t2}', end='')
+cont = 3
+
+while cont <= n:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' -> FIM!')
