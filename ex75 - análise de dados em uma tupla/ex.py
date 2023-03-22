6353b5f776160500b20c9357ff51537009261727
valdir-amaral <valdir.amaral58@gmail.com>
2023-03-22 10:22:38 -0300

    análise de dados em tupla

---
+++ ex75 - análise de dados em uma tupla/ex.py
@@ -0,0 +1,20 @@
+""" Exercício Python 075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+
+A) Quantas vezes apareceu o valor 9.
+B) Em que posição foi digitado o primeiro valor 3.
+C) Quais foram os números pares. """
+
+numeros = (int(input('Digite um número: ')),
+           int(input('Digite outro número: ')),
+           int(input('Digite mais um número: ')),
+           int(input('Digite o último número: ')),)
+
+print(f'\nVocê digitou os números {numeros}')
+print(f'O número 9 apareceu {numeros.count(9)} vezes')
+if 3 in numeros:
+    print(f'O número 3 apareceu na {numeros.index(3)+1}ª posição')
+else:
+    print('O número 3 não foi digitado!')
+for n in numeros:
+    if n % 2 == 0:
+        print(n, end=' ')
