92220d64fb3d80ff4d232b766c887fa3a9de77f0
valdir-amaral <valdir.amaral58@gmail.com>
2023-03-29 10:53:23 -0300

    mais sobre matriz em python

--- ex87 - mais sobre matriz em python/ex.py
+++ ex87 - mais sobre matriz em python/ex.py
@@ -9,15 +9,16 @@ for i in range(0, 3):
     for j in range(0, 3):
         n = int(input(f'Digite um número para [{i}, {j}]: '))
         matriz[i].append(n)
-        if n % 2 == 0: soma_pares += n
-        if i == 1: 
+        if n % 2 == 0: soma_pares += n # soma os pares
+
+        if i == 1: # analisa o maior da segunda linha
             if j == 0: maior_segunda = n
             else:
                 if n > maior_segunda: maior_segunda = n
-        if j == 2:
+
+        if j == 2: # soma a terceira coluna
             soma_terceira += n
                 
-
 for i in range(0, 3):
     for j in range(0, 3):
         print(f'[{matriz[i][j]:^5}]', end='')
