7d3d163c8d4adf137ad5a802eece53db83da8955
valdir-amaral <valdir.amaral58@gmail.com>
2023-03-29 10:53:05 -0300

    mais sobre matriz em python

---
+++ ex87 - mais sobre matriz em python/ex.py
@@ -0,0 +1,28 @@
+"""Exercício Python 087: Aprimore o desafio anterior, mostrando no final:
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha. """
+
+matriz = [[], [], []]
+soma_pares = maior_segunda = soma_terceira = 0
+for i in range(0, 3):
+    for j in range(0, 3):
+        n = int(input(f'Digite um número para [{i}, {j}]: '))
+        matriz[i].append(n)
+        if n % 2 == 0: soma_pares += n
+        if i == 1: 
+            if j == 0: maior_segunda = n
+            else:
+                if n > maior_segunda: maior_segunda = n
+        if j == 2:
+            soma_terceira += n
+                
+
+for i in range(0, 3):
+    for j in range(0, 3):
+        print(f'[{matriz[i][j]:^5}]', end='')
+    print()
+
+print(f'\nA soma dos pares é {soma_pares}')
+print(f'A soma dos valores da terceira coluna é {soma_terceira}')
+print(f'O maior valor da segunda linha é {maior_segunda}')
