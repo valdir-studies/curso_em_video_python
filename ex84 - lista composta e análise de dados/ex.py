9d705daf4c45eee589bf535e2d530f920ecb263b
valdir-amaral <valdir.amaral58@gmail.com>
2023-03-29 10:01:31 -0300

    lista composta e análise de dados

--- ex84 - lista composta e análise de dados/ex.py
+++ ex84 - lista composta e análise de dados/ex.py
@@ -8,15 +8,13 @@ C) Uma listagem com as pessoas mais leves.
 pessoas = []
 maior = menor = 0
 while True:
-    p_nome = input('Nome: ')
+    p_nome = input('Nome: ').capitalize()
     p_peso = float(input('Peso (kg): '))
     if len(pessoas) == 0: 
         maior = menor = p_peso
     else:
-        if p_peso > maior:
-            maior = p_peso
-        if p_peso < menor:
-            menor = p_peso
+        if p_peso > maior: maior = p_peso
+        if p_peso < menor: menor = p_peso
     pessoas.append([p_nome, p_peso])
     opt = input('Quer continuar? [S/N]: ')
     if opt in 'nN': break
